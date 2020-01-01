d2d46a4865c63e242195c0c6d9bfdbeea4ef241d
slycoder2000 <sly@slycoder.com>
2020-01-01 05:21:57 -0700

    Study Library Initial Commit

---
+++ workshop/StudyLibrary/vuejs/vfs/src/components/Accordion.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<p>{{item.title + ' INSIDE'}}</p>
+		<p @click="toggle=!toggle">Details</p>
+		<p v-if="toggle">{{item.description}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Accordion',
+
+	props: ['item'],
+
+	data: function() {
+		return {
+			toggle: false
+		};
+	}
+};
+</script>
+
